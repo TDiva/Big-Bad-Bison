46a81968c2fcc7185705b64251d66f217117b09e
alina <zabavskaya.alina@gmail.com>
2016-02-11 23:02:54 +0300

    didnt work

--- src/solver/ShortPathSolver.java
+++ src/solver/ShortPathSolver.java
@@ -1,8 +1,11 @@
 package solver;
 
+import javafx.util.Pair;
 import model.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by TDiva on 2/11/16.
@@ -20,6 +23,25 @@ public class ShortPathSolver extends AbstractSolver {
     @Override
     public Result run() {
 
+        int distance = 0;
+        int min_distance_to_order = Integer.MAX_VALUE;
+        Map<Integer,Pair<Order,Warehouse>> turns = new HashMap<>();
+        Pair<Order,Warehouse> temp;
+
+
+        for(Order o: orders){
+            Pair<Integer,Integer> minDinstanceWarhouse;
+            for(Warehouse w : warehouses){
+                distance = (int) Math.ceil(Math.sqrt(Math.abs(o.getxDeliverPos()-w.getxPos())^2 + Math.abs(o.getyDeliverPos()-w.getyPos())^2));
+                if(distance < min_distance_to_order){
+                    min_distance_to_order = distance;
+                    minDinstanceWarhouse = new Pair<>(distance,w.getId());
+                }
+                temp = new Pair<>(o,w);
+                turns.put(min_distance_to_order,temp);
+
+            }
+        }
         return new Result();
     }
 }
