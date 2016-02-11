6a5d531d5fba6f5c266066ecd4ab4fddfa262231
VladAbramov <abramov.vlad182@gmail.com>
2016-02-11 21:57:06 +0300

    Model

--- src/model/Product.java
+++ src/model/Product.java
@@ -6,4 +6,28 @@ package model;
 public class Product {
     private int typeCode;
     private int weight;
+
+    public Product() {
+    }
+
+    public Product(int typeCode, int weight) {
+        this.typeCode = typeCode;
+        this.weight = weight;
+    }
+
+    public int getTypeCode() {
+        return typeCode;
+    }
+
+    public void setTypeCode(int typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
