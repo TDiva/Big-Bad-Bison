6c219a9da2329236a46307a4506a9397066bfa1e
VladAbramov <abramov.vlad182@gmail.com>
2016-02-11 21:45:52 +0300

    REFACTORING

--- src/solver/VladSolver.java
+++ src/solver/VladSolver.java
@@ -13,12 +13,12 @@ import java.util.List;
  */
 public class VladSolver extends AbstractSolver {
 
-    public VladSolver(List<Drone> drones, List<Order> orders, List<Warehouse> warehouses, Integer r, Integer c) {
-        super(drones, orders, warehouses, r, c);
+    public VladSolver(List<Drone> drones, List<Order> orders, List<Warehouse> warehouses, Integer r, Integer c, Integer m) {
+        super(drones, orders, warehouses, r, c, m);
     }
 
-    public VladSolver(int r, int c) {
-        super(r, c);
+    public VladSolver(int r, int c, int m) {
+        super(r, c, m);
     }
 
     @Override
